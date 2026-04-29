eb5d9941f280269e1d620458f002cf7148eb18dc
leonardo <leopereirasilva86@gmail.com>
2026-04-28 21:15:26 -0300

    first commit

---
+++ Konversor/Controllers/PorcentagemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Konversor.Controllers
+{
+    public class PorcentagemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
