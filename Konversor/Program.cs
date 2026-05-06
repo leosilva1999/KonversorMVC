c17108421ab3a2cb9a226263b40d83717933dcef
UCOB - APLAC - CAP - Ti1 <cap.ti1@adventistas.org>
2026-05-06 16:33:57 -0300

    Porcentagem View added to sidebar menu

--- Konversor/Program.cs
+++ Konversor/Program.cs
@@ -1,7 +1,11 @@
+using Konversor.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IPorcentagemService, PorcentagemService>();
 
 var app = builder.Build();
 
